2a57aa09a09005267db46637e6278ef37d30026a
nesabenjamin <nesabenjamin@gmail.com>
2016-10-16 18:49:52 +0530

    comments session

--- server.js
+++ server.js
@@ -13,12 +13,16 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi2.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi2.png'));
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/ui/line.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'line.png'));
+app.get('/ui/nesh.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'nesh.jpg'));
+});
+
+app.get('/ui/cooltext70.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'cooltext70.png'));
 });
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
