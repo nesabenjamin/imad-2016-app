e2b70c3be4bbb244ce92bed299a924b07d06ad1b
nesabenjamin <nesabenjamininfo@gmail.com>
2016-11-02 09:29:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,20 +87,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var Pool = new Pool(config);
-app.get('/test-tb', function (req, res) {
-  
-  pool.query('SELECT * FROM test', function(err, result){
-
-  		if(err){
-  			res.status(500).send(err.toString());
-  		} else {
-  			res.send(JSON.stringify(result));
-  		}
-  });
-
-});
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
@@ -117,6 +103,19 @@ app.get('/counter', function (req, res) {
   res.send(counter.toString());
 });
 
+var Pool = new Pool(config);
+app.get('/test-tb', function (req, res) {
+  
+  pool.query('SELECT * FROM test', function(err, result){
+
+  		if(err){
+  			res.status(500).send(err.toString());
+  		} else {
+  			res.send(JSON.stringify(result));
+  		}
+  });
+
+});
 var names = [];
 app.get('/submit_name/:name', function (req, res) {
   var name = req.params.name;
