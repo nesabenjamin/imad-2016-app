cb630d2a66563f8a077f67d0a06900c5abd4df58
nesabenjamin <nesabenjamininfo@gmail.com>
2016-11-11 17:48:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
 var config = {
 	user:'nesabenjamin',
 	database:'nesabenjamin',
@@ -151,6 +152,16 @@ var counter=0;
 app.get('/counter', function (req, res) {
 	counter++;
   res.send(counter.toString());
+});
+
+function hash(input,salt){
+  var hashed = crypto.pbkdf2Sync(input,salt,1000,512,'sha512');
+  return hashed.toString('hex')   
+}
+
+app.get('/hash/:input',function(req, res){
+  var hashedString = hash(req.params.input,'a-random-string');
+  res.send(hashedString);
 });
 
 var pool = new Pool(config);
