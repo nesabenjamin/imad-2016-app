30a3c5b104eb5cb3e4da9a5882e9955b87162be6
nesabenjamin <nesabenjamininfo@gmail.com>
2016-11-19 19:08:04 +0530

    [imad-console] Updates ui/articles.js

--- ui/articles.js
+++ ui/articles.js
@@ -8,6 +8,7 @@
                     <input type="button" onclick="show_comments()" value="Post as a guest" class= "but2"/></p>`;}
 
             function show_comments(){
+                alert("---------");
                 var name = document.getElementById("name");
                 var p1 = document.getElementById("p1");
                     p1.innerHTML ="<hr/>" +name.value;
