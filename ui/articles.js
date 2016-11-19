7cbe54057a21c675e35133a6c94652f75ac6c949
nesabenjamin <nesabenjamininfo@gmail.com>
2016-11-19 20:20:38 +0530

    [imad-console] Updates ui/articles.js

--- ui/articles.js
+++ ui/articles.js
@@ -1,11 +1,12 @@
-            function submit(){
+function submit(){
                 //alert(".value");
                 var box = document.getElementById("comment_box");
                 box.innerHTML = `
                 <p>Name <input type="text" id="name"/></p>
                 <p> comments <textarea id="txtarea" name="comments" rows="5" cols="30" placeholder="Type in your comment here..."></textarea></p>
                  <p class= "center">         
-                    <input type="button" onclick="show_comments()" value="Post as a guest" class= "but2"/></p>`;}
+                    <input type="button" onclick="show_comments()" value="Post as a guest" class= "but2"/></p>`;
+}
 
 function show_comments(){
     var currentArticleTitle = window.location.pathname.split('/')[2];
@@ -44,30 +45,35 @@ function show_comments(){
     request.send(null);
 }
 
-    function add_comments(){
-        alert("---------");
-    	//alert(document.getElementById("name").value);
-    	var request = new XMLHttpRequest();
-    	request.onreadystatechange = function(){
-    		if(request.readyState === XMLHttpRequest.DONE){
-    			if(request.status === 200){
-    				var names  = request.responseText;
-    				names = JSON.parse(names);
-    				var list = '';
-    				for (var i=0; i<names.length; i++){
+function add_comments(){
+    alert("---------");
+	//alert(document.getElementById("name").value);
+	var request = new XMLHttpRequest();
+	request.onreadystatechange = function(){
+		if(request.readyState === XMLHttpRequest.DONE){
+			if(request.status === 200){
+				var names  = request.responseText;
+				names = JSON.parse(names);
+				var list = '';
+				for (var i=0; i<names.length; i++){
 
-    					list+= '<p>'+names[i]+'</p>';
-    				}
-    				var p = document.getElementById("p2");
-            			p.innerHTML = list;
-    			}
-    		}
-    	};
+					list+= '<p>'+names[i]+'</p>';
+				}
+				var p = document.getElementById("p2");
+        			p.innerHTML = list;
+			}
+		}
+	};
 
-        var nameInput = document.getElementById("name");
-        var name = nameInput.value;
-        request.open('GET','http://127.0.0.1:8080/submit_name?name='+name,true);
-        request.send(null);
-    }
+    var nameInput = document.getElementById("name");
+    var name = nameInput.value;
+    request.open('GET','http://127.0.0.1:8080/submit_name?name='+name,true);
+    request.send(null);
+    
+}
+    
+    
+    
+    
     //var template =`<a href="http://127.0.0.1:8080/login#"><button class="but left" id="logIN">Log <span class="bold" id="slogIN">IN</span></button></a>`;
                 //document.getElementById("div2").innerHTML=template;
